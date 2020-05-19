0a3bc9e980488a99271290898c193c8df255749a
taniot <gaetano.frascolla@gmail.com>
2020-05-19 10:39:36 +0200

    feat: manage leaderbord

--- src/pages/leaderboard/leaderboard.component.js
+++ src/pages/leaderboard/leaderboard.component.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Container } from '../pages.styles'
 
 class LeaderBoard extends Component {
   render() {
-    return <div>LeaderBoard</div>
+    const { users } = this.props
+    return (
+      <Container>
+        {users.map((user) => (
+          <div key={user.id} id={user.id}>
+            {user.id}
+          </div>
+        ))}
+      </Container>
+    )
   }
 }
 
-export default LeaderBoard
+const mapStateToProps = ({ users }) => {
+  const userScore = (user) =>
+    Object.keys(user.answers).length + user.questions.length
+  return {
+    users: Object.values(users).sort((a, b) => userScore(b) - userScore(a)),
+  }
+}
+
+export default connect(mapStateToProps)(LeaderBoard)
